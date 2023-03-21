e7fe38ed09e7b7b54359ca56e6860ab78b1f594b
Glitchyi <advaithnarayanan8@gmail.com>
2023-03-21 11:52:19 +0530

    about fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,38 +4,7 @@ import Card from "./Card";
 export default function Schedule({ props }) {
   return (
     <div className="h-full w-full flex flex-wrap gap-5 justify-center sm:pl-16">
-      <Card
-        name={"Adrin"}
-        date={"Lead Developer"}
-        link={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
-        button={true}
-        bg={"cardback"}
-        cardback={"cardback"}
-      />
-      <Card
-        name={"Advaith"}
-        date={"Boss Man"}
-        link={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
-        button={true}
-        bg={"cardback"}
-        cardback={"cardback"}
-      />
-      <Card
-        name={"Joshua"}
-        date={"Lead Designer"}
-        link={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
-        button={true}
-        bg={"cardback"}
-        cardback={"cardback"}
-      />
-      <Card
-        name={"Anand"}
-        date={"Lead Designer"}
-        link={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
-        button={true}
-        bg={"cardback"}
-        cardback={"cardback"}
-      />
+      <h1 className="drummer text-9xl text-white mt-80 sm:text-lg">COMING SOON</h1>
     </div>
   );
 }
