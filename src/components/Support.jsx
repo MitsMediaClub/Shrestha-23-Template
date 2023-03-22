2024cd9c3226f8fed65b8a8832505474a801f56f
Adrin7113 <adrinjosect@gmail.com>
2023-03-22 23:31:01 +0530

    AFHOGU

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -9,22 +9,85 @@ const Support = () => {
           support@shreshta.tech
         </a>
       </h1>
-      <div className="w-1/2 h-auto mt-6">
-        <h1 className="text-white drummer text-3xl mb-10 font-semibold">
-          Abhijith
-        </h1>
-        <h1 className="text-white drummer text-3xl mb-10 font-semibold">
-          Ambady
-        </h1>
-        <h1 className="text-white drummer text-3xl mb-10 font-semibold">
-          Ebin John
-        </h1>
-        <h1 className="text-white drummer text-3xl mb-10 font-semibold">
-          Antony
-        </h1>
-        <h1 className="text-white drummer text-3xl mb-10 font-semibold">
-          Advaith
-        </h1>
+      <div className="w-full h-auto mt-6">
+        <div className="flex justify-between mb-10">
+          <div className="flex flex-col ">
+            <h1 className="text-white drummer text-3xl  font-semibold ">
+              S Abhijit
+            </h1>
+            <h1 className="text-white drummer text-3xl  font-semibold ">
+              8301972674
+            </h1>
+          </div>
+          <h1 className="text-white drummer text-3xl mb-10 font-semibold w-1/2">
+            (SHRESHTA Technical Head)
+          </h1>
+        </div>
+        <div className="flex justify-between mb-10">
+          <div className="flex flex-col ">
+            <h1 className="text-white drummer text-3xl  font-semibold ">
+              Ambady S
+            </h1>
+            <h1 className="text-white drummer text-3xl  font-semibold ">
+              9656387648
+            </h1>
+          </div>
+          <h1 className="text-white drummer text-3xl mb-10 font-semibold w-1/2">
+            (SHRESHTA Cultural Head)
+          </h1>
+        </div>
+        <div className="flex justify-between mb-10">
+          <div className="flex flex-col ">
+            <h1 className="text-white drummer text-3xl  font-semibold ">
+              Ebin John
+            </h1>
+            <h1 className="text-white drummer text-3xl  font-semibold ">
+              9072333014
+            </h1>
+          </div>
+          <h1 className="text-white drummer text-3xl mb-10 font-semibold w-1/2">
+            (Media Club Lead)
+          </h1>
+        </div>
+        <div className="flex justify-between mb-10">
+          <div className="flex flex-col ">
+            <h1 className="text-white drummer text-3xl  font-semibold ">
+              Antony Maliakkal
+            </h1>
+            <h1 className="text-white drummer text-3xl  font-semibold ">
+              7558014901
+            </h1>
+          </div>
+          <h1 className="text-white drummer text-3xl mb-10 font-semibold w-1/2">
+            (Student Council Secretary)
+          </h1>
+        </div>
+        <div className="flex justify-between mb-10">
+          <div className="flex flex-col ">
+            <h1 className="text-white drummer text-3xl  font-semibold ">
+              Madhav Manoj
+            </h1>
+            <h1 className="text-white drummer text-3xl  font-semibold ">
+              9400466616
+            </h1>
+          </div>
+          <h1 className="text-white drummer text-3xl mb-10 font-semibold w-1/2">
+            (Student Council Chairman)
+          </h1>
+        </div>
+        <div className="flex justify-between mb-10">
+          <div className="flex flex-col ">
+            <h1 className="text-white drummer text-3xl  font-semibold ">
+              Advaith Narayanan
+            </h1>
+            <h1 className="text-white drummer text-3xl  font-semibold ">
+              9400354625
+            </h1>
+          </div>
+          <h1 className="text-white drummer text-3xl mb-10 font-semibold w-1/2">
+            (Media Club Technical Lead)
+          </h1>
+        </div>
       </div>
     </div>
   );
