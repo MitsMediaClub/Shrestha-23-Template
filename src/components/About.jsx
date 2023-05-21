b8952a1f467e2266efbf34d8eb3236b2284c110b
Adrin7113 <adrinjosect@gmail.com>
2023-05-22 00:31:45 +0530

    FINAL RLY FR

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,19 @@ export default function Schedule({ props }) {
           <span className="text-[#14CAF4]">designers</span>.
         </h1>
         <div className="flex justify-center items-start w-full pt-16 gap-10">
-          <div className="flex flex-col gap-1 w-[200px]">
+          <div
+            onMouseEnter={() => {
+              const card = document.getElementById("Adrin");
+              card.classList.toggle("opacity-0");
+              card.classList.toggle("opacity-100");
+            }}
+            onMouseLeave={() => {
+              const card = document.getElementById("Adrin");
+              card.classList.toggle("opacity-0");
+              card.classList.toggle("opacity-100");
+            }}
+            className="flex flex-col gap-1 w-[200px] relative"
+          >
             <img
               src="/Adrin.png"
               alt="ADRIN JOSE C T"
@@ -23,8 +35,39 @@ export default function Schedule({ props }) {
             <h1 className="text-white quicksand text-sm">
               FULL STACK DEVELOPER
             </h1>
+            <div
+              id="Adrin"
+              className="wBE absolute bottom-[83px] left-0 w-full h-12 transition-opacity ease-in-out duration-700 opacity-0 hover:opacity-100 flex justify-center items-center gap-5"
+            >
+              <a href="https://github.com/Adrin7113">
+                <img
+                  src="/github.svg"
+                  alt="Github Logo"
+                  className="w-6 h-6 hover:scale-110 transition-all ease-in-out"
+                />
+              </a>
+              <a href="https://www.linkedin.com/in/adrin-jose-c-t-101a70249/">
+                <img
+                  src="/linkedin.svg"
+                  alt="Linkedin Logo"
+                  className="w-6 h-6 hover:scale-110 transition-all ease-in-out"
+                />
+              </a>
+            </div>
           </div>
-          <div className="flex flex-col gap-1 w-[200px]">
+          <div
+            onMouseEnter={() => {
+              const card = document.getElementById("Advaith");
+              card.classList.toggle("opacity-0");
+              card.classList.toggle("opacity-100");
+            }}
+            onMouseLeave={() => {
+              const card = document.getElementById("Advaith");
+              card.classList.toggle("opacity-0");
+              card.classList.toggle("opacity-100");
+            }}
+            className="flex flex-col gap-1 w-[200px] relative"
+          >
             <img
               src="/Advaith.png"
               alt="ADVAITH NARAYANAN"
@@ -32,17 +75,79 @@ export default function Schedule({ props }) {
             />
             <h1 className="text-white drummer text-xl">ADVAITH NARAYANAN</h1>
             <h1 className="text-white quicksand text-sm">DEVOPS ENGINEER</h1>
+            <div
+              id="Advaith"
+              className="wBE absolute bottom-[83px] left-0 w-full h-12 transition-opacity ease-in-out duration-700 opacity-0 hover:opacity-100 flex justify-center items-center gap-5"
+            >
+              <a href="https://github.com/Glitchyi">
+                <img
+                  src="/github.svg"
+                  alt="Github Logo"
+                  className="w-6 h-6 hover:scale-110 transition-all ease-in-out"
+                />
+              </a>
+              <a href="https://www.linkedin.com/in/advaith-narayanan-a72152214/">
+                <img
+                  src="/linkedin.svg"
+                  alt="Linkedin Logo"
+                  className="w-6 h-6 hover:scale-110 transition-all ease-in-out"
+                />
+              </a>
+            </div>
           </div>
-          <div className="flex flex-col gap-1 w-[200px]">
+          <div
+            onMouseEnter={() => {
+              const card = document.getElementById("Anandu");
+              card.classList.toggle("opacity-0");
+              card.classList.toggle("opacity-100");
+            }}
+            onMouseLeave={() => {
+              const card = document.getElementById("Anandu");
+              card.classList.toggle("opacity-0");
+              card.classList.toggle("opacity-100");
+            }}
+            className="flex flex-col gap-1 w-[200px] relative"
+          >
             <img
               src="/Anandu.png"
               alt="ANANDA KRISHNAN M"
               className=" w-[200px] h-[200px]"
             />
             <h1 className="text-white drummer text-xl">ANANDA KRISHNAN</h1>
-            <h1 className="text-white quicksand text-sm">DESIGN LEAD</h1>
+            <h1 className="text-white quicksand text-sm">UI/UX DESIGNER</h1>
+            <div
+              id="Anandu"
+              className="wBE absolute bottom-[83px] left-0 w-full h-12 transition-opacity ease-in-out duration-700 opacity-0 hover:opacity-100 flex justify-center items-center gap-5"
+            >
+              <a href="mailto:anandakrishnan3537@gmail.com">
+                <img
+                  src="/mail.svg"
+                  alt="Mail Logo"
+                  className="w-6 h-6 hover:scale-110 transition-all ease-in-out"
+                />
+              </a>
+              <a href="https://www.linkedin.com/in/ananda-krishnan-96a86324a/">
+                <img
+                  src="/linkedin.svg"
+                  alt="Linkedin Logo"
+                  className="w-6 h-6 hover:scale-110 transition-all ease-in-out"
+                />
+              </a>
+            </div>
           </div>
-          <div className="flex flex-col gap-1 w-[200px] items-start justify-center">
+          <div
+            onMouseEnter={() => {
+              const card = document.getElementById("Joshua");
+              card.classList.toggle("opacity-0");
+              card.classList.toggle("opacity-100");
+            }}
+            onMouseLeave={() => {
+              const card = document.getElementById("Joshua");
+              card.classList.toggle("opacity-0");
+              card.classList.toggle("opacity-100");
+            }}
+            className="flex flex-col gap-1 w-[200px] items-start justify-center relative"
+          >
             <img
               src="/Joshua.png"
               alt="JOSHUA VARGHESE"
@@ -52,10 +157,41 @@ export default function Schedule({ props }) {
               <h1 className="text-white drummer text-xl">JOSHUA</h1>
               <h1 className="text-white drummer text-xl">VARGHESE</h1>
             </div>
-            <h1 className="text-white quicksand text-sm">PROJECT MANAGER</h1>
+            <h1 className="text-white quicksand text-sm">UI/UX ENGINEER</h1>
+            <div
+              id="Joshua"
+              className="wBE absolute bottom-[83px] left-0 w-full h-12 transition-opacity ease-in-out duration-700 opacity-0 hover:opacity-100 flex justify-center items-center gap-5"
+            >
+              <a href=" https://github.com/infinitytmbots">
+                <img
+                  src="/github.svg"
+                  alt="Github Logo"
+                  className="w-6 h-6 hover:scale-110 transition-all ease-in-out"
+                />
+              </a>
+              <a href="https://www.linkedin.com/in/joshuavarghese1/">
+                <img
+                  src="/linkedin.svg"
+                  alt="Linkedin Logo"
+                  className="w-6 h-6 hover:scale-110 transition-all ease-in-out"
+                />
+              </a>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
+}
+{
+  /*
+  https://github.com/Adrin7113
+  https://github.com/Glitchyi
+  https://github.com/infinitytmbots
+  mailto:anandakrishnan3537@gmail.com
+  https://www.linkedin.com/in/adrin-jose-c-t-101a70249/
+  https://www.linkedin.com/in/advaith-narayanan-a72152214/
+  https://www.linkedin.com/in/joshuavarghese1/
+  https://www.linkedin.com/in/ananda-krishnan-96a86324a/
+  */
 }
